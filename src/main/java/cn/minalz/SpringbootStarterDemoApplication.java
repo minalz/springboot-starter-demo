c6c5ac3c7f8bcc9477a03281897cf08b101d43d0
zhouwei <zhouwei@hywintech.com>
2023-03-16 17:17:48 +0800

    first commit

---
+++ src/main/java/cn/minalz/SpringbootStarterDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.minalz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterDemoApplication.class, args);
+	}
+
+}
