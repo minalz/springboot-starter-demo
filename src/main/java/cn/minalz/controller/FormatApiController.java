c72ca6d433c1bb559280a892f1556f7f7dc041e2
zhouwei <zhouwei@hywintech.com>
2023-03-22 13:17:00 +0800

    feat: update

--- src/main/java/cn/minalz/controller/FormatApiController.java
+++ src/main/java/cn/minalz/controller/FormatApiController.java
@@ -3,21 +3,23 @@ package cn.minalz.controller;
 import cn.minalz.domain.User;
 import cn.minalz.template.HelloFormatTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 格式控制器
- * @author minalz
- * @since 2023/03/17 00:15:20
+ * 对外（API）暴露格式控制器
+ * @author zhouwei
+ * @date 2023/3/21 13:39
  */
+@RequestMapping("/api")
 @RestController
-public class FormatController {
+public class FormatApiController {
 
     @Autowired
     private HelloFormatTemplate helloFormatTemplate;
 
-    @GetMapping("/format")
+    @PostMapping ("/format")
     public String format() {
         User user = new User();
         user.setName("minalz");
